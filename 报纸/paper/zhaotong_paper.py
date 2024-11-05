683d649aee794bf07903d520501921467c145b37
BM_lyh <2347948121@qq.com>
2024-11-05 15:40:07 +0800

    报纸——债权——政务公开11.05上午更新

--- 报纸/paper/zhaotong_paper.py
+++ 报纸/paper/zhaotong_paper.py
@@ -246,6 +246,8 @@ def get_zhaotong_paper_old_old(paper_time, queue_id, webpage_id):
     if response.status_code == 200:
         content = response.content.decode()
         html_1 = etree.HTML(content)
+        if html_1 is None:
+            raise Exception(f'该日期没有报纸')
         # 获取所有版面的的链接
         all_bm = html_1.xpath("//tr/td[@class='default']/a[@id='pageLink']")
         for bm in all_bm:
@@ -360,4 +362,4 @@ def get_zhaotong_paper(paper_time, queue_id, webpage_id):
         # print('使用新方法')
         get_zhaotong_paper_new(paper_time, queue_id, webpage_id)
 
-# get_zhaotong_paper('2013-12-04', 111, 1111)
+# get_zhaotong_paper('2021-05-13', 111, 1111)
