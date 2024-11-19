dc2a01966ed1b3d1503d00fbf0bb4ab8f80e0c4b
BM_lyh <2347948121@qq.com>
2024-11-19 20:40:08 +0800

    报纸——债权——政务公开11.19上午更新

--- AI接口/识别保证人.py
+++ AI接口/识别保证人.py
@@ -17,46 +17,75 @@ def guarantor(model_name=deepseek_chat):
 
     # 查询数据
     cursor_test.execute(
-        "SELECT id, guarantee_new, mortgagor_new, collateral_new, content FROM tmp_paper_collateral"
+        "SELECT id, guarantee_new, mortgagor_new, collateral_new, content FROM tmp_paper_collateral1"
     )
     rows = cursor_test.fetchall()
     for id, guarantee_new, mortgagor_new, collateral_new, content in rows:
-        if not (guarantee_new and mortgagor_new and collateral_new):
-            content = re.sub(r'\n', '', content)
-            print(id, guarantee_new, content)
+        content = re.sub(r'\n', '', content)
+        print(id, guarantee_new, content)
+        if not guarantee_new:
             try:
-                a, b, guarantee_new = model_name(
-                    content, system_content="请从中提取保证人(包含公司和个人,当担保人没有特殊说明时默认为保证人)， 抵押人/质押人(提供质押物/抵押物的公司或个人)，抵押物/质押物信息，单个主体以','分割，不要输出其他无关字段,没有保证人返回'空'，没有抵押人/质押人返回'空'，没有抵押物/质押物返回'空'，严格按照执行。案例：保证人:某某有限公司,李某某,王某某; 抵押人/质押人:某某公司，某某人; 抵押物/质押物:位于某处房产,某工厂;(每项用';'结束)注意：此案例为借鉴数据，请不要引用里面的数据，不要将其他与保证人无关的主体放在保证人里面。")
-                print(guarantee_new)
-                guarantor = ''.join(re.findall(r'保证人:(.*?);', guarantee_new))
-                mortgage_person = ''.join(re.findall(r'抵押人/质押人:(.*?);', guarantee_new))
-                mortgage_goods = ''.join(re.findall(r'抵押物/质押物:(.*?);', guarantee_new))
+                a, b, guarantor = model_name(
+                    content,
+                    system_content="请从中提取保证人(包含公司和个人,当担保人没有特殊说明时默认为保证人。保证人之间如有明确亲属关系，在保证人后面追加（）说明。)主体，单个主体以','分割，不要输出其他无关字段,没有保证人返回'空'，严格按照执行。案例：保证人:某某有限公司,李某某,王某某;(每项用';'结束)注意：此案例为借鉴数据，请不要引用里面的数据，不要将其他与保证人无关的主体放在保证人里面，抵押人或质押人不应被视为保证人。")
+                guarantor = re.sub(r'保证人：', '', guarantor)
+                guarantor = re.sub(r'保证人:', '', guarantor)
+                guarantor = re.sub(r';', '', guarantor)
                 if not guarantor:
                     guarantor = "空"
-                if not mortgage_person:
-                    mortgage_person = "空"
-                if not mortgage_goods:
-                    mortgage_goods = "空"
                 if guarantor == "某某有限公司,李某某,王某某":
                     guarantor = "空"
-                if mortgage_person == "某某公司":
-                    mortgage_person = "空"
-                if mortgage_goods == "位于某处房产,某工厂":
-                    mortgage_goods = "空"
 
                 cursor_test1 = conn_test.cursor()
-                insert_sql = "UPDATE tmp_paper_collateral SET guarantee_new = %s WHERE id = %s"
+                insert_sql = "UPDATE tmp_paper_collateral1 SET guarantee_new = %s WHERE id = %s"
                 cursor_test1.execute(insert_sql, (guarantor, id))
                 conn_test.commit()
 
-                cursor_test1 = conn_test.cursor()
-                insert_sql = "UPDATE tmp_paper_collateral SET mortgagor_new = %s WHERE id = %s"
-                cursor_test1.execute(insert_sql, (mortgage_person, id))
-                conn_test.commit()
+
+            except:
+                print("发生错误")
+                continue
+        if not mortgagor_new:
+            try:
+                a, b, mortgagor = model_name(
+                    content,
+                    system_content="从中提取抵押人/质押人主体名称，单个主体以','分割，不要输出其他无关字段,没有抵押人/质押人返回'空'，严格按照执行。案例：抵押人/质押人:某某有限公司,李某某,王某某;(每项用';'结束)注意：此案例为借鉴数据，请不要引用里面的数据。文中没有明确说明担保人是抵押人/质押人时，担保人不可作为抵押人/质押人。")
+                mortgagor = re.sub(r'抵押人/质押人：', '', mortgagor)
+                mortgagor = re.sub(r'抵押人/质押人:', '', mortgagor)
+                mortgagor = re.sub(r';', '', mortgagor)
+                if not mortgagor:
+                    print(1111)
+                    mortgagor = "空"
+                if mortgagor == "某某有限公司,李某某,王某某":
+                    print(2222)
+                    mortgagor = "空"
 
                 cursor_test1 = conn_test.cursor()
-                insert_sql = "UPDATE tmp_paper_collateral SET collateral_new = %s WHERE id = %s"
-                cursor_test1.execute(insert_sql, (mortgage_goods, id))
+                insert_sql = "UPDATE tmp_paper_collateral1 SET mortgagor_new = %s WHERE id = %s"
+                cursor_test1.execute(insert_sql, (mortgagor, id))
+                conn_test.commit()
+
+            except:
+                print("发生错误")
+                continue
+        if not collateral_new:
+            try:
+                a, b, collateral = model_name(
+                    content,
+                    system_content="请从中提取抵押物/质押物(当担保物没有特殊说明时默认为抵押物/质押物)信息，单个主体以','分割，不要输出其他无关字段,没有抵押物/质押物返回'空'，严格按照执行。案例：抵押物/质押物:位于某处房产,某工厂;(每项用';'结束)注意：此案例为借鉴数据，请不要引用里面的数据，不要将其他与抵押物/质押物无关的主体放在抵押物/质押物里面，抵质押物情况中有主体的也要保存。")
+                collateral = re.sub(r'抵押物/质押物：', '', collateral)
+                collateral = re.sub(r'抵押物/质押物:', '', collateral)
+                collateral = re.sub(r'抵押物：|质押物：', '', collateral)
+                collateral = re.sub(r'抵押物:|质押物:', '', collateral)
+                collateral = re.sub(r';', '', collateral)
+                if not collateral:
+                    collateral = "空"
+                if collateral == "位于某处房产,某工厂":
+                    collateral = "空"
+
+                cursor_test1 = conn_test.cursor()
+                insert_sql = "UPDATE tmp_paper_collateral1 SET collateral_new = %s WHERE id = %s"
+                cursor_test1.execute(insert_sql, (collateral, id))
                 conn_test.commit()
             except:
                 print("发生错误")
